f3c2fceae2502334ca8253f1a9daab4419000000
awarbler <awoodfor@byui.edu>
2023-08-22 11:53:58 -0500

    added helloWorld

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World");
+            Console.ReadLine();
+
+            // build errors Say No  
+            // usually red lines under the code 
+            // make sure your code is in the code blocks
+            // error : expected means that it is missing semicolon 
+            // check error the name console does not exist in the current context
+            // so make sure that we are using capital letter 
+        }
+    }
+}
